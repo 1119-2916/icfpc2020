9f8a15a09be49b8b1a5d080bfef446b16325009f
Luma <markermails@gmail.com>
2020-07-19 08:56:16 +0900

    is -> isinstance

--- parse.py
+++ parse.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-functions = None
-state = nil
-vector = Vect(0, 0)
-
 
 class Expr:
     def __init__(self):
@@ -14,9 +10,6 @@ class Atom(Expr):
     def __init__(self, Name):
         super().__init__()
         self.Name = Name
-
-
-nil = Atom("nil")
 
 
 class Ap(Expr):
@@ -35,13 +28,20 @@ class Vect:
 def PARSE_FUNCTIONS(filename):
     with open(filename, 'r') as f:
         line = f.readline()
-        
+        print(line)
 
 
-def asNum(n): # Atom(Expr) -> number
-    if (n is Atom)
+def PARSE_NUMBER(s):
+    return int(s)
+
+
+nil = Atom("nil")
+
+
+def asNum(expr): # Atom(Expr) -> number
+    if (isinstance(expr, Atom)):
         return PARSE_NUMBER(n.Name)
-    ERROR("not a number")
+    raise Exception("not a number")
 
 
 def evalCons(a, b): # (Expr, Expr) -> Expr
@@ -57,7 +57,7 @@ def eval(expr): # Expr -> Expr
     initialExpr = expr
     while (True):
         result = tryEval(expr)
-        if (result == expr)
+        if (result == expr):
             initialExpr.Evaluated = result
             return result
         expr = result
@@ -69,12 +69,12 @@ def tryEval(expr): # Expr -> Expr
 
     if (expr.Evaluated != None):
         return expr.Evaluated
-    if (expr is Atom && functions[expr.Name] != None):
+    if (isinstance(expr, Atom) and functions[expr.Name] != None):
         return functions[expr.Name]
-    if (expr is Ap):
+    if (isinstance(expr, Ap)):
         fun = eval(expr.Fun)
         x = expr.Arg
-        if (fun is Atom): # 1 arg
+        if (isinstance(fun, Atom)): # 1 arg
             if (fun.Name == "neg"):
                 return Atom(-asNum(eval(x)))
             if (fun.Name == "i"):
@@ -87,10 +87,10 @@ def tryEval(expr): # Expr -> Expr
                 return Ap(x, t)
             if (fun.Name == "cdr"):
                 return Ap(x, f)
-        if (fun is Ap):
+        if (isinstance(fun, Ap)):
             fun2 = eval(fun.Fun)
             y = fun.Arg
-            if (fun2 is Atom): # 2 args
+            if (isinstance(fun2, Atom)): # 2 args
                 if (fun2.Name == "t"):
                     return y
                 if (fun2.Name == "f"):
@@ -102,15 +102,15 @@ def tryEval(expr): # Expr -> Expr
                 if (fun2.Name == "div"):
                     return Atom(asNum(eval(y)) / asNum(eval(x)))
                 if (fun2.Name == "lt"):
-                    return asNum(eval(y)) < asNum(eval(x)) ? t : f
+                    return t if asNum(eval(y)) < asNum(eval(x)) else f
                 if (fun2.Name == "eq"):
-                    return asNum(eval(x)) == asNum(eval(y)) ? t : f
+                    return t if asNum(eval(x)) == asNum(eval(y)) else f
                 if (fun2.Name == "cons"):
                     return evalCons(y, x)
-            if (fun2 is Ap):
+            if (isinstance(fun2, Ap)):
                 fun3 = eval(fun2.Fun)
                 z = fun2.Arg
-                if (fun3 is Atom): # 3 args
+                if (isinstance(fun3, Atom)): # 3 args
                     if (fun3.Name == "s"):
                         return Ap(Ap(z, x), Ap(y, x))
                     if (fun3.Name == "c"):
@@ -131,14 +131,21 @@ def interact(state, event): # (flag, newstate, data) -> (newstate, multipledraw)
         return (newState, data)
     return interact(newState, SEND_TO_ALIEN_PROXY(data))
 
-
-if __name__ == '__main__':
-
-    Map<string, Expr> functions = PARSE_FUNCTIONS("galaxy.txt")
-
+def main():
+    state = nil
+    vector = Vect(0, 0)
     while(True):
         click = Ap(Ap(cons, Atom(vector.X)), Atom(vector.Y))
         (newState, images) = interact(state, click)
         PRINT_IMAGES(images)
         vector = REQUEST_CLICK_FROM_USER()
         state = newState
+
+
+# これを動くようにしてほしい
+functions = {}
+print(eval(Ap(Atom('i'), Atom('1'))))
+
+
+# functions = PARSE_FUNCTIONS("galaxy.txt")
+
