2ce9888547b3e01f123a165c836776409ef7c262
ferin-15 <ys236236@gmail.com>
2020-07-19 18:54:28 +0900

    guiの見た目を調整

--- view.py
+++ view.py
@@ -9,9 +9,9 @@ class print_galaxy(tkinter.Frame):
         self.pack()
         
         master.title(u"galaxy")
-        master.geometry("800x800")
+        master.geometry("1600x900")
 
-        self.canvas = tkinter.Canvas(master, width = 800, height = 800)
+        self.canvas = tkinter.Canvas(master, width = 1600, height = 1000)
         self.canvas.place(x=0, y=0)
         self.canvas.bind('<ButtonPress-1>', self.click)
 
@@ -20,18 +20,18 @@ class print_galaxy(tkinter.Frame):
         self.images = []  # to hold the newly created image
 
     def redraw(self, input_images=[[(1,1),(1,2),(1,3)],[(1,3),(1,4)]]):
-        offset_x = 400
-        offset_y = 400       
+        offset_x = 800
+        offset_y = 500       
         for (image, i) in zip(input_images, range(len(input_images))):
-            self.create_rectangle_with_alpha(0, 0, 800, 800, fill="black", outline="", alpha=0.4)
+            self.create_rectangle_with_alpha(0, 0, 1600, 1000, fill="black", outline="", alpha=0.4)
             for vec in image:
                 col = self.hsv_to_colorcode(1.0 / len(input_images) * i, 1.0, 0.8)
-                self.create_rectangle_with_alpha(offset_x+vec[0]*10, offset_y+vec[1]*10, offset_x+(vec[0]+1)*10, offset_y+(vec[1]+1)*10, fill=col, outline="", alpha=0.4)
+                self.create_rectangle_with_alpha(offset_x+vec[0]*6, offset_y+vec[1]*6, offset_x+(vec[0]+1)*6, offset_y+(vec[1]+1)*6, fill=col, outline="", alpha=0.4)
 
     def click(self, event):
         self.images = []
-        self.clickx = (event.x-400)//10
-        self.clicky = (event.y-400)//10
+        self.clickx = (event.x-800)//6
+        self.clicky = (event.y-500)//6
         self.master.quit()
         print(self.clickx, self.clicky)
         return ((event.x-400)//10, (event.y-400)//10)
